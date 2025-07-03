4f60e218522ab6411ec478e48452e012d696cb42
serverxdt <runningserver94@gmail.com>
2025-07-04 03:38:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ html_content = '''
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> 
      <style>
         body {
-            font-family: Arial, sans-serif;
+            
             background-color: black;
       }
     </style>
